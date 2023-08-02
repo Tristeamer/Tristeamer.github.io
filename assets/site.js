443e7d2d45e6a459cf5053dba582788182489881
Quintailius Barnacle Jones Jr <94586110+Tristeamer@users.noreply.github.com>
2023-08-01 21:37:22 -0600

    d

--- assets/site.js
+++ assets/site.js
@@ -1,15 +1,16 @@
 //site scripting stuff
 
 var x = 0;
-var sitetext = "site text <br>";
+var sitel1 = "Welcome to my site!! :) (this page is still a work in progress but that's probably pretty evident lmao)";
 var sitivt = 5;
 
 function site() {
-  if (x < sitetext.length) {
-    document.getElementById("site").innerHTML += sitetext.charAt(x);
+  if (x < sitel1.length) {
+    document.getElementById("site").innerHTML += sitel1.charAt(x);
     x++;
     setTimeout(site, sitivt);
   }
 }
+
 
 setTimeout(() => {site();}, 3300);
